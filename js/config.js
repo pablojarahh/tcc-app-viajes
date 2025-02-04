a75e741902f1d933978e9290154c97e69f77c3dd
Pablo Jara <pablo.jarahh@gmail.com>
2025-02-03 21:22:19 -0300

    cognito ids2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Fjts19jYl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7ffoadbntd92q97ad9ndvckljs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2dc8rkqd3luufuq4k74q2apq8q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
